a4728451291451bdd70ed0a14eef502205fdfdf7
avnish <97375647+avnlsh@users.noreply.github.com>
2024-10-11 21:20:13 +0530

    Update Pyramid Generator using JS.js

--- Pyramid Generator using JS.js
+++ Pyramid Generator using JS.js
@@ -1,1 +1,14 @@
+function generatePyramid(height) {
+    for (let i = 1; i <= height; i++) {
+        // Generate spaces
+        let spaces = ' '.repeat(height - i);
+        // Generate hashes
+        let hashes = '#'.repeat(i * 2 - 1);
+        // Log the pyramid row
+        console.log(spaces + hashes + spaces);
+    }
+}
+
+// Call the function with a desired height
+generatePyramid(5);
 
